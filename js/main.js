6dee01129187f6efc5daf43f42f332f94d6909e8
a-pritchard11 <a_pritchard148854@fanshaweonline.ca>
2026-03-02 10:22:35 -0500

    Initial Commit
    Added all necessary files for the work, as detailed within the README

---
+++ js/main.js
@@ -0,0 +1,60 @@
+console.log("JavaScript File is linked");
+
+//variables
+
+const labels = document.querySelectorAll(".label")
+const targetZones = document.querySelectorAll(".target-zone");
+let currentDraggedElement = null;
+
+// add variable for reset button
+
+// functions
+
+function dragStart() {
+    console.log("Started Dragging");
+    // whatever the user is dragging, store it under "currentDraggedElement"
+    currentDraggedElement = this;
+}
+
+function dragOver(e) {
+    e.preventDefault();
+    console.log("drag over called")
+}
+
+function dropped(e) {
+    
+    if(this.children.length>=1) {
+        return
+    }
+
+    e.preventDefault();
+    console.log("Dropped")
+
+    // drop the piece
+    this.appendChild(currentDraggedElement);
+
+    //reset the reference
+    currentDraggedElement = null;
+
+    
+}
+
+
+
+// Event Listeners
+
+labels.forEach(label => {
+    label.addEventListener("dragstart", dragStart);
+})
+
+targetZones.forEach(zone => {
+    zone.addEventListener("dragover", dragOver);
+    zone.addEventListener("drop", dropped);
+})
+
+//listen for click event, call a reset function
+
+//function reset() {
+    //collect all the labels and put them back
+    //check all the target zones/loop through them, see IF the dropzone has a label in it. if it does
+//}
